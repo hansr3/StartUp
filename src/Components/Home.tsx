a27185b92c909c4e9c2d0912a7e52a262b1e5118
Hans <htja0002@student.monash.edu>
2024-02-02 23:27:21 +1100

    added nav bar

---
+++ src/Components/Home.tsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+
+const Home = () => {
+    
+    return(
+        
+        <div className="home">
+            
+            <Header/>
+        </div>
+        
+    
+    );
+    
+}
+
+export default Home;
